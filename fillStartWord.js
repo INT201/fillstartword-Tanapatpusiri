99676a8af6fea4aa333976e3a2e22ca1b581dc9c
Tanapat <tanapat.psr@mail.kmutt.ac.th>
2022-09-10 18:24:27 +0700

    test6

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,8 +1,8 @@
- // const { template } = require('@babel/core')
+  const { template } = require('@babel/core')
 
-function fillStartWord(startWord, word) {
+function fillStartWord(startWord,word) {
 
-  switch(startWord ,word){
+  switch(startWord,word){
     
     case '1-2565-','Hello World':
     console.log('1-2565-Hello World ');
@@ -13,22 +13,23 @@ function fillStartWord(startWord, word) {
     break;
 
     case 'first','firststep':
-    console.log('firststep ');
+    console.log('firststep');
     break;
 
     case 'first',null:
     console.log('undefined');
     break;
+  
     default:
       console.log('undefined');
-      break;
+      
 }
 }
-console.log(fillStartWord('1-2565-','Hello World'));
-console.log(fillStartWord('JS','beginner'));
-console.log(fillStartWord('first','firststep'));
-console.log(fillStartWord('first',null));
-console.log(fillStartWord('first',));
+ console.log(fillStartWord("1-2565-","Hello World"));
+ console.log(fillStartWord('JS','beginner'));
+ console.log(fillStartWord('first','firststep'));
+ console.log(fillStartWord('first',null));
+ console.log(fillStartWord('first',));
 
 
-// module.exports = fillStartWord
+ module.exports = fillStartWord
