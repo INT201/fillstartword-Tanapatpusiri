9337155590b544222961490aa3ad8d88998a92ea
Tanapat <tanapat.psr@mail.kmutt.ac.th>
2022-09-10 15:39:59 +0700

    Test1

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,24 @@
-const { template } = require('@babel/core')
+// const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+
+  if(startWord ='1-2565-', word ='Hello World'){
+    console.log('1-2565-Hello World ');
+    
+  } else if (startWord ="JS", word="beginner") {
+    console.log("JSbeginner");
+
+  }  else if (startWord ="first", word="firststep") {
+    console.log("firststep");
+
+  } else if (startWord ="first", word=undefined) {
+    console.log(undefined);
+
+  } else {
+    console.log(undefined);
+  }
 }
-module.exports = fillStartWord
+
+console.log(fillStartWord('JS','firststep'));
+
+// module.exports = fillStartWord
